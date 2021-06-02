f3d9f26eb7534e6ae00c1a5809ee2ec83e0f493d
vivek <vivekyamsani@gmail.com>
2021-06-02 15:35:44 +0530

    inital commit first subtask in normal mode

---
+++ genUser.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+var=30
+for u in  {"sysAd_","webDev_","appDev_"}
+do
+	for i in {0..2}
+	do 
+		for j in {1..9}
+		do 
+			pass="$u$i$j@07"
+			sudo useradd -m "$u$i$j"
+			echo "$u$i$j:$pass" | sudo chpasswd 
+		done
+		
+	done
+	pass1=$u$var@07
+	sudo useradd -m "$u$var"
+	echo "$u$var:$pass1" | sudo chpasswd 
+done
