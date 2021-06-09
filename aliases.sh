f5ed786d8d6cf4a7569427b6870ce31386651384
vivek <vivekyamsani@gmail.com>
2021-06-09 21:16:44 +0530

    adding aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "alias genUser='~/genUser.sh'" >> ~/.bashrc
+echo "alias permit='~/permit.sh'" >> ~/.bashrc
+echo "alias schedule='~/schedule.sh'" >> ~/.bashrc
+echo "alias attendance='~/attendance.sh'" >> ~/.bashrc
